36fea24b2a7d5b716fab1653b348fbd3a1160dcd
Kim Salinas <ksalinas.bavc@gmail.com>
2010-05-04 19:25:44 -0700

    added google maps

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def google_map(address, options = {})
+      size = options[:size] ||= "512x512"
+      color = options[:color] ||= "red"
+      image_tag "http://maps.google.com/maps/api/staticmap?size=#{size}&maptype=roadmap&markers=size:large|color:#{color}|#{address}&sensor=false"
+    end
 end
